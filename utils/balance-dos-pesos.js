ba5a1de5bcaa6fb3fbfc269b4cf8b80a16ac998d
Wako <dulo.rodrigo@gmail.com>
2019-12-10 22:56:13 -0600

    Dos pesos

---
+++ utils/balance-dos-pesos.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const diferencia = require('./diferencia')
+
+const balanceDosPesos = (mapa, izquierda, derecha) => {
+  let answer = []
+
+  derecha.forEach((peso, index) => {
+    const contadorPeso = mapa.get(derecha[index])
+    mapa.set(derecha[index], contadorPeso - 1)
+    const dif = diferencia(izquierda) + derecha[index]
+
+    // Un peso de cada lado
+    if (mapa.get(dif) > 0) {
+      answer = derecha[index] > dif ? [dif, derecha[index]] : [derecha[index], dif]
+      index = derecha.length
+    } else if (
+      derecha[index] + derecha[index + 1] + izquierda[0] === izquierda[1]
+    ) {
+      // Dos peso de cada lado
+      answer = [derecha[index], derecha[index + 1]]
+      index = derecha.length
+    }
+  })
+
+  return answer
+}
+
+module.exports = balanceDosPesos
